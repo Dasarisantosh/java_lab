6ee05a4739b75daf93cbcb73173f4af6b941277a
Dasarisantosh <santoshdasari740@gmail.com>
2026-01-28 12:39:36 +0530

    Add files via upload

---
+++ week1/totalaverage.java
@@ -0,0 +1,23 @@
+class totalaverage 
+{
+	static int a,b;
+	static int total;
+	static double average;
+	static void calculate()
+	{
+		total=a+b;
+		average=total/2.0;
+	}
+public static void main(String[] args) 
+	{
+		//command-line arguments
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		calculate();
+		System.out.println("First Number="+a);
+		System.out.println("Second Number="+b);
+		System.out.println("Total="+total);
+		System.out.println("Average ="+average);
+
+	}
+}
